49e59bd2ed2eb27b3fa1ab7d7896dec3b1022010
Shivam Gupta <sgs.shivam99@outlook.com>
2025-02-04 17:21:02 +0530

    commit

--- decorator.py
+++ decorator.py
@@ -1,3 +1,6 @@
-import functions_in_python
-result=functions_in_python.add(2,3)
-print("result:",result)
+import sys
+sys.path.append('c:/Users/Shivam Gupta/OneDrive/Documents/Shivam_Developement/PYTHON')
+
+import functions_in_python # type: ignore
+result = functions_in_python.add(2, 3)
+print("result:", result)
