11a4278edc5873b40e3b28484e59a81e3e8d6807
Shivam Gupta <sgs.shivam99@outlook.com>
2025-05-09 20:49:07 +0530

    commit

--- python_tutorial/Inventory_Managemeny_System_In_Package/inventory_management/reporting.py
+++
@@ -1,8 +0,0 @@
-from .inventory import inventory_data
-
-def generate_inventory_report():
-    """Generate a report of current inventory."""
-    print("\nInventory Report:")
-    for item, quantity in inventory_data.items():
-        print(f"{item}: {quantity}")
-    print()
